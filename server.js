34c63fd1a30fd3d857c88529162f4eca24553778
Boymurotov7 <93852739+Boymurotov7@users.noreply.github.com>
2022-07-29 21:19:49 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(todoRoutes)
+
+async function start() {
+  try {
+    await mongoose.connect(
+        'mongodb+srv://shukur:wwwwwww@cluster0.8ithriu.mongodb.net/blog?retryWrites=true&w=majority',
+    )
+    app.listen(PORT, () => {
+      console.log('Server has been started...')
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
